47eff7afae7c181432c27166bc313a21f0863bac
TheRABU <mdfazlerabbi114@gmail.com>
2024-06-09 23:44:31 +0600

    added a purchase button in view details page

--- src/pages/SingleFoodPage/SingleFoodPage.jsx
+++ src/pages/SingleFoodPage/SingleFoodPage.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 import { MdOutlineReviews } from "react-icons/md";
 const SingleFoodPage = () => {
   const foodData = useLoaderData();
@@ -100,6 +100,31 @@ const SingleFoodPage = () => {
                     <div className="flex items-center gap-x-3">
                       <span>Quantity</span>
                       <button className="btn btn-square">{Quantity}</button>
+                      <Link
+                        href="#_"
+                        className="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-2 border-purple-500 rounded-full shadow-md group"
+                      >
+                        <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-purple-500 group-hover:translate-x-0 ease">
+                          <svg
+                            className="w-6 h-6"
+                            fill="none"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
+                              d="M14 5l7 7m0 0l-7 7m7-7H3"
+                            ></path>
+                          </svg>
+                        </span>
+                        <span className="absolute flex items-center justify-center w-full h-full text-purple-500 transition-all duration-300 transform group-hover:translate-x-full ease">
+                          Purchase
+                        </span>
+                        <span className="relative invisible">Purchase</span>
+                      </Link>
                     </div>
                   </div>
                 </div>
@@ -113,7 +138,7 @@ const SingleFoodPage = () => {
 
                 <hr className="my-6 border-gray-200 " />
 
-                <div>
+                <div className="">
                   <h3 className="text-blue-500 capitalize ml-2">Price</h3>
                   <span className="badge badge-info text-white font-semibold">
                     {Price}
