d7f21ea5d9fcf04fec8bd9c9f257ebfae38b6769
TheRABU <mdfazlerabbi114@gmail.com>
2024-06-10 19:51:21 +0600

    purchase now page's form value implemented

--- src/pages/FoodPurchasePage/PurchaseNowPage.jsx
+++ src/pages/FoodPurchasePage/PurchaseNowPage.jsx
@@ -6,11 +6,39 @@ const PurchaseNowPage = () => {
   const foodDetail = useLoaderData();
   const { FoodImage, FoodName, Quantity, Price } = foodDetail;
   const { user } = useContext(AuthContext);
+
+  // handle order function
+
+  const handleOrder = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = user.displayName;
+    const email = user.email;
+    const date = form.date.value;
+    const area = form.area.value;
+    const state = form.state.value;
+    const city = form.city.value;
+    const postCode = form.postCode.value;
+
+    const orderValue = {
+      name,
+      email,
+      date,
+      area,
+      state,
+      city,
+      postCode,
+      FoodName,
+      Price,
+    };
+    console.log(orderValue);
+  };
+
   return (
     <>
-      <div className="flex-row lg:flex items-center justify-center py-12 px-8">
+      <div className="flex-row lg:flex items-center justify-center py-12 px-6 lg:px-16">
         <div>
-          <div className="mb-5 px-3 md:px-20">
+          {/* <div className="mb-5 px-3 md:px-20">
             <div className="card bg-base-100 shadow-2xl">
               <figure className="px-10 pt-10">
                 <img src={FoodImage} alt="Food Image" className="rounded-xl" />
@@ -26,10 +54,32 @@ const PurchaseNowPage = () => {
                 </Link>
               </div>
             </div>
+          </div> */}
+          <div className="max-w-md mx-auto rounded-md shadow-md bg-gray-900 text-gray-100">
+            <img
+              src={FoodImage}
+              alt=""
+              className="object-cover object-center w-full rounded-t-md h-72 bg-gray-500"
+            />
+            <div className="flex flex-col justify-between p-6 space-y-8">
+              <div className="space-y-2">
+                <h2 className="text-3xl font-semibold tracking-wide">
+                  {FoodName}
+                </h2>
+                <p className="text-gray-100">{Price}</p>
+                <p className="text-gray-100">Left {Quantity}</p>
+              </div>
+
+              <Link to="/all-foods">
+                <button className="btn w-full mx-auto px-10 bg-violet-50">
+                  Browse more
+                </button>
+              </Link>
+            </div>
           </div>
         </div>
         <div className="mx-auto w-full max-w-[550px] bg-white">
-          <form>
+          <form onSubmit={handleOrder}>
             <div className="mb-5">
               <label
                 htmlFor="name"
@@ -141,7 +191,7 @@ const PurchaseNowPage = () => {
                   <div className="mb-5">
                     <input
                       type="text"
-                      name="post-code"
+                      name="postCode"
                       id="post-code"
                       placeholder="Post Code"
                       className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
